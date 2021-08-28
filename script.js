47d769365d65321715cc337340b9676456ee9d67
TF3sun <61900697+TF3sun@users.noreply.github.com>
2021-08-28 17:22:40 +0900

    add js file

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+for(var i = 1; i <= 9; i++){
+	document.write("<div>");
+	document.write("<h3>" + i + "단<h3>");
+	for(var j=1; j<=9; j++){
+		document.write(i + "x" + j + "=" + i*j + "<br>");
+	}
+	document.write("</div>");
+}
+*/
